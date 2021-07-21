e8be681907f6d27a6bec3f6516a18d37205fb704
AlexShyshko <shish1988@mail.ru>
2021-07-21 23:23:20 +0300

    feat: write function filtering errors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,55 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let expressionWithoutSpaces = expr.split(' ').join('');
+
+    let bracketsArr = ['(', ')'];//priority 3
+    let mulDivArr = ['*', '/'];//priority 2
+    let plusMinusArr = ['+', '-'];//priority 1
+
+    /*function checking errors*/
+
+    const funcCheckErr = function(str) {
+        let i = 0;
+        let result = '';
+        let openBracket = 0;
+        let closeBracket = 0;
+        for (i = 0; i < str.length; i++) {
+            if (str[i] === ')' && openBracket === 0) {
+                result = 'BracketErr';
+                break;
+            } else if (str[i] === '(') {
+                openBracket++;
+            } else if (str[i] === ')') {
+                closeBracket++;
+            } else if (str[i] === '/' && str[i + 1] === '0') {
+                result = 'ZeroErr';
+                break;
+            }
+        }
+        if (openBracket !== closeBracket && result !== 'ZeroErr') {
+            result = 'BracketErr';
+        } else if (result === 'BracketErr') {
+            result = 'BracketErr';
+        } else if (result === 'ZeroErr') {
+            result = 'ZeroErr';
+        } else {
+            result = str;
+        }
+        return result;
+    }
+
+    let expressionCheckedErrors = funcCheckErr(expressionWithoutSpaces);
+    
+    if (expressionCheckedErrors === 'ZeroErr') {
+        throw "TypeError: Division by zero.";
+    } else if (expressionCheckedErrors === 'BracketErr') {
+        throw "ExpressionError: Brackets must be paired";
+    }
+
+    let expressionWithoutErrors = expressionCheckedErrors;
+    return expressionWithoutErrors;
+
 }
 
 module.exports = {
